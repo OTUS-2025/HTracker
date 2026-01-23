88d5be0023ca04431729c687ec11ef1ca59825b2
Max V. Shumakov <shumakovmv@gmail.com>
2026-01-23 09:13:02 +0300

    Add PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
+import PrimeVue from 'primevue/config'
+import Material from '@primeuix/themes/material'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Material,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
 
 app.mount('#app')
